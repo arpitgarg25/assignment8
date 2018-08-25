3b78f0cd024db3cde11881efb206fc374b171cc0
arpitgarg25 <42351800+arpitgarg25@users.noreply.github.com>
2018-08-25 12:16:32 +0530

    Add files via upload

---
+++ assignment8.py
@@ -0,0 +1,105 @@
+#Q=1
+class circle:
+    def __init__ (self,r):
+        self.radius=r
+    def getArea(self):
+        self.area=3.14*self.radius*self.radius
+        return self.area
+    def getCircumference(self):
+        self.c=2*3.14*self.radius
+        return self.c
+r=int(input("Enter radius of circle "))
+c_1=circle(r)
+print("Area of circle is: ",c_1.getArea())
+print("Circumference of circle is: ", c_1.getCircumference())
+
+
+#Q=2
+class student:
+    def __init__(self,name,rno):
+        self.name=name
+        self.rollno=rno
+    def setAge(self,age):
+        self.age=age
+    def setMarks(self,marks):
+        self.marks=marks
+    def display(self):
+        print("Name:",self.name)
+        print("Roll No.:",self.rollno)
+        print("Age:",self.age)
+        print("Marks:",self.marks)
+name=input("name: ")
+rno=int(input("rollno: "))
+age=int(input("age"))
+marks=int(input("marks"))
+s1=student(name,rno)
+s1.setAge(age)
+s1.setMarks(marks)
+s1.display() 
+
+#Q=3
+class Temperature:
+    def convertFahrenheit(self,c):
+        f=((c*9)/5)+32
+        return f
+    def convertCelsius(self,f):
+        c=(f-32)/1.8
+        return c
+t=Temperature()
+temp1=int(input("Enter temperature in Celcius "))
+temp2=int(input("Enter temperature in Fahrenheit "))
+print(temp1,"in Fahrenheit is",t.convertFahrenheit(temp1)," and ",temp2,"in Celclius is",t.convertCelsius(temp2))
+
+
+#Q=4
+class MovieDetails:
+    def add(self,name,year,rating):
+        self.artistname=name
+        self.yearofrelease=year
+        self.rating=rating
+    def display(self):
+        print("Artist Name: ",self.artistname,"\nYear of release:",self.yearofrelease,"\nRating:",self.rating)
+    
+m=MovieDetails()
+name=input("Enter name of artist ")
+year=int(input("Enter year of release "))
+rating=int(input("Enter rating "))
+m.add(name,year,rating)
+m.display()
+
+#Q=5
+class Animal:
+    def animal_attribute(self):
+        print("My attribute is: Lion")
+class Tiger(Animal):
+    def tiger_attr(self):
+        print(" ")
+a=Animal()
+t=Tiger()
+t.animal_attribute() 
+
+#Q=6
+'''The output will be
+A B
+A B
+'''
+
+#Q=7
+class shape:
+    def __init__(self,l,b):
+        self.length=l
+        self.breadth=b
+    def area(self):
+        self.area=self.length*self.breadth
+        return self.area
+class rect(shape):
+    def __init__(self,l,b):
+        self.length=l
+        self.breadth=b
+class square(shape):
+    def __init__(self,s):
+        self.length=s
+        self.breadth=s
+r=rect(10,20)
+s=square(10)
+print("Area of rectangle:",r.area(),"and area of square:",s.area())
